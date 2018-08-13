997b040d4d0f747c1db98f68623481becbf062c6
lodely <lodely0306@gmail.com>
2018-08-13 12:03:12 +0800

    connect mysql and add column

--- spider_movie_scrapy/pipelines.py
+++ spider_movie_scrapy/pipelines.py
@@ -5,42 +5,55 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
+from scrapy.conf import settings
 
 class SpiderMovieScrapyPipeline(object):
     def process_item(self, item, spider):
         return item
 
 class MoviePipeline(object):
-    def process_item(self,item,spider):
-        '''
-        将爬取的信息保存到mysql
-        '''
-        # 将item里的数据拿出来
-        name = item['name']
-        translateName = item['translateName']
-        title = item['title']
 
-        # 和本地的newsDB数据库建立连接
-        db = pymysql.connect(
-            host='localhost',  # 连接的是本地数据库
-            port='3306',
-            user='root',  # 自己的mysql用户名
-            passwd='root',  # 自己的密码
-            db='spider_movie',  # 数据库的名字
-            charset='utf8',  # 默认的编码方式：
-            cursorclass=pymysql.cursors.DictCursor)
+    def process_item(self, item, spider):
+
+        host = settings['MYSQL_HOST']
+        user = settings['MYSQL_USER']
+        psd = settings['MYSQL_PASSWD']
+        db = settings['MYSQL_DB']
+        c=settings['CHARSET']
+        port=settings['MYSQL_PORT']
+        #数据库连接
+        con=pymysql.connect(host=host,user=user,passwd=psd,db=db,charset=c,port=port)
+        #数据库游标
+        cue=con.cursor()
+        print("mysql connect succes")#测试语句，这在程序执行时非常有效的理解程序是否执行到这一步
 
         try:
-            # 使用cursor()方法获取操作游标
-            cursor = db.cursor()
-            # SQL 插入语句
-            sql = "INSERT INTO movies(name,translateName,title) \
-                  VALUES ('%s', '%s', '%s')" % (name,translateName,title)
-            # 执行SQL语句
-            cursor.execute(sql)
-            # 提交修改
-            db.commit()
-        finally:
-            # 关闭连接
-            db.close()
+            cue.execute("""insert into movies
+                        (name, translateName, title, imgUrl, time, place, category, language, subtitle, dbScore, length, director, star, info, magneticLink, downloadUrl)
+                        values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                        [item['name'],
+                         item['translateName'],
+                         item['title'],
+                         item['imgUrl'],
+                         item['time'],
+                         item['place'],
+                         item['category'],
+                         item['language'],
+                         item['subtitle'],
+                         item['dbScore'],
+                         item['length'],
+                         item['director'],
+                         item['star'],
+                         item['info'],
+                         item['magneticLink'],
+                         item['downloadUrl'],
+                         ])
+            print("insert success")#测试语句
+
+        except Exception as e:
+            print('Insert error:',e)
+            con.rollback()
+        else:
+            con.commit()
+        con.close()
         return item
